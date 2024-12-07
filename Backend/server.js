59506b5d85f872ba5286938605f6823d5ab18b05
Aryan Daftari <aryandaftari7@gmail.com>
2024-12-08 01:14:48 +0530

    Setting Backend things

--- Backend/server.js
+++ Backend/server.js
@@ -1,8 +1,9 @@
 const http = require("http");
 const app = require("./app");
+const PORT = process.env.PORT || 3000;
 
 const server = http.createServer(app);
 
-server.listen(3000, () => {
-    console.log("Server is running on port 3000");
+server.listen(PORT, () => {
+    console.log("Server is running on port",PORT);
 });
