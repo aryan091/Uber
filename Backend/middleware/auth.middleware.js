e95307057d26b62a2256e6de1cd4b68ca4665152
Aryan Daftari <aryandaftari7@gmail.com>
2024-12-08 09:34:03 +0530

    Middleware for Auth

---
+++ Backend/middleware/auth.middleware.js
@@ -0,0 +1,24 @@
+const userModel = require("../models/user.model");
+const jwt = require("jsonwebtoken");
+const bycrypt = require("bcryptjs");
+const { model } = require("mongoose");
+
+module.exports.authUser = async (req, res, next) => {
+    try {
+        const token = req.headers.authorization.split(' ')[1] || req.cookies.token;
+        if(!token)
+        {
+            return res.status(401).json({error: "Unauthorized"});
+        }
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        const user = await userModel.findById(decoded._id);
+        if (!user) {
+            throw new Error();
+        }
+        req.user = user;
+        req.token = token;
+        next();
+    } catch (err) {
+        res.status(401).json({error: "Unauthorized"});
+    }
+}
