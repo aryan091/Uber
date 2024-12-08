5a79274d59f5e0ed72418d799fbdac625f46543d
Aryan Daftari <aryandaftari7@gmail.com>
2024-12-08 09:24:34 +0530

    User Login Complete

--- Backend/controllers/user.controller.js
+++ Backend/controllers/user.controller.js
@@ -22,3 +22,26 @@ const hashedPassword = await userModel.hashPassword(password);
         res.status(500).json({ error: err.message });
     }
 }; 
+
+module.exports.login = async (req, res) => {
+
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+    try {
+        const {email, password} = req.body;
+        const user = await userModel.findOne({ email }).select("+password");
+        if (!user) {
+            return res.status(401).json({ error: "Invalid email or password" });
+        }
+        const isMatch = await user.comparePassword(password);
+        if (!isMatch) {
+            return res.status(401).json({ error: "Invalid email or password" });
+        }
+        const token = await user.generateAuthToken();
+        res.status(200).json({ token, user });
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+};
--- Backend/routes/user.routes.js
+++ Backend/routes/user.routes.js
@@ -5,4 +5,9 @@ const userController = require("../controllers/user.controller");
 
 router.post("/register",[ body("email").isEmail().withMessage("Please enter a valid email"),body("fullname.firstname").isLength({min:3}).withMessage("First Name must be at least 3 characters long"),body("fullname.lastname").isLength({min:3}).withMessage("Last Name must be at least 3 characters long"),body("password").isLength({min:6}).withMessage("Password must be at least 6 characters long")],userController.register); 
 
+router.post("/login",[
+    body("email").isEmail().withMessage("Please enter a valid email"),
+    body("password").isLength({min:6}).withMessage("Password must be at least 6 characters long")
+],userController.login);
+
 module.exports = router;
