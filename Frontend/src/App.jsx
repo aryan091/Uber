f70c7db4088f6bd6fc77a143c1cbdc8acc08da1a
Aryan Daftari <aryandaftari7@gmail.com>
2024-12-09 18:04:03 +0530

    Routing Done

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import { Route , Routes} from 'react-router-dom'
 
 const App = () => {
   return (
-    <div>Uber</div>
+    <div>
+      <Routes>
+      <Route path="/" element={<Home />} /> 
+      <Route path="/userLogin" element={<UserLogin />} />
+      <Route path="/userSignUp" element={<UserSignUp />} />
+      <Route path="/captainLogin" element={<CaptainLogin />} />
+      <Route path="/captainSignUp" element={<CaptainSignUp />}  /> 
+      </Routes>
+    </div>
   )
 }
 
