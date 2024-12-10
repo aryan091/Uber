60420b7dbcf63e850ae158868272b5477b4264a3
Aryan Daftari <aryandaftari7@gmail.com>
2024-12-11 00:31:44 +0530

    Routes Added

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,5 +1,10 @@
 import React from 'react'
 import { Route , Routes} from 'react-router-dom'
+import Home from './pages/Home'
+import UserLogin from './pages/UserLogin' 
+import UserSignUp from './pages/UserSignUp'
+import CaptainLogin from './pages/CaptainLogin'
+import CaptainSignUp from './pages/CaptainSignUp'
 
 const App = () => {
   return (
