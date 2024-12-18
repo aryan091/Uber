2f9f1383306989363b7e10f0f399e7a31f331d2a
Aryan Daftari <aryandaftari7@gmail.com>
2024-12-19 00:10:56 +0530

    Added UseEffect so it will work properly on Loading

--- Frontend/src/pages/UserProtectWrapper.jsx
+++ Frontend/src/pages/UserProtectWrapper.jsx
@@ -1,13 +1,15 @@
-import React , {useContext} from 'react'
+import React , {useEffect} from 'react'
 import { useNavigate } from 'react-router-dom';
-import {UserDataContext} from "../context/UserContext";
 
 const UserProtectWrapper = ({children}) => {
 
     const token = localStorage.getItem('token')
     const navigate = useNavigate()
-    console.log(user)
-    if(!token ) navigate("/userLogin")
+
+    useEffect(() => {
+        if(!token ) navigate("/userLogin")
+    }, [token])
+
 
 
   return (
