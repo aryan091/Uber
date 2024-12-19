8b0c7690b27e45edc0c1b3fafb2fa15f967e42c3
Aryan Daftari <aryandaftari7@gmail.com>
2024-12-19 21:24:13 +0530

    Enhanced Security in Higher Order Components

--- Frontend/src/pages/CaptainProtectWrapper.jsx
+++ Frontend/src/pages/CaptainProtectWrapper.jsx
@@ -1,16 +1,43 @@
 import React , {useEffect} from 'react'
 import { useNavigate } from 'react-router-dom';
+import {CaptainDataContext} from "../context/CaptainContext"
+import axios from 'axios'
+
 
 const CaptainProtectWrapper = ({children}) => {
 
     const token = localStorage.getItem('token')
     const navigate = useNavigate()
+    const {captain , setCaptain} = React.useContext(CaptainDataContext)
+    const [loading , setLoading] = useState(true)
+
+    
 
     useEffect(() => {
         if(!token ) navigate("/captainLogin")
+
+          axios.get(`${import.meta.env.VITE_BASE_URL}/captains/profile` , 
+            {
+              headers : {
+                "Authorization" : `Bearer ${token}`
+                        }
+            })
+            .then((res) => {
+              if(res.status === 200) 
+              {
+              setCaptain(res.data.captain)        
+              setLoading(false)
+              }
+          }).catch((err) => {console.log(err)
+            localStorage.removeItem('token')
+          navigate("/captainLogin")}
+        )
     }, [token])
 
 
+    
+
+  if(loading) return <h1>Loading...</h1>
 
   return (
     <>
--- Frontend/src/pages/UserProtectWrapper.jsx
+++ Frontend/src/pages/UserProtectWrapper.jsx
@@ -1,13 +1,35 @@
 import React , {useEffect} from 'react'
 import { useNavigate } from 'react-router-dom';
+import {UserDataContext} from "../context/UserContext"
+import axios from 'axios'
 
 const UserProtectWrapper = ({children}) => {
 
     const token = localStorage.getItem('token')
     const navigate = useNavigate()
+    const {user , setUser} = React.useContext(UserDataContext)
+    const [loading , setLoading] = useState(true)
 
     useEffect(() => {
         if(!token ) navigate("/userLogin")
+
+          axios.get(`${import.meta.env.VITE_BASE_URL}/users/profile` , 
+            {
+              headers : {
+                "Authorization" : `Bearer ${token}`
+                        }
+            })
+            .then((res) => {
+              if(res.status === 200) 
+              {
+              setUser(res.data.user)        
+              setLoading(false)
+              }
+          }).catch((err) => {console.log(err)
+            localStorage.removeItem('token')
+          navigate("/userLogin")
+        }
+        )
     }, [token])
 
 
