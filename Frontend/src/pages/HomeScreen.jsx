fc827a15ca2e266859d19cb8fee5976caeb333c0
Aryan Daftari <aryandaftari7@gmail.com>
2024-12-24 01:16:56 +0530

    Completing HomeScreen

--- Frontend/src/pages/HomeScreen.jsx
+++ Frontend/src/pages/HomeScreen.jsx
@@ -1,15 +1,18 @@
 import React, { useRef, useState } from "react";
 import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
-import 'remixicon/fonts/remixicon.css'
+import "remixicon/fonts/remixicon.css";
 import LocationSearchPanel from "../components/LocationSearchPanel";
+import VehiclePanel from "../components/VehiclePanel";
 
 const HomeScreen = () => {
   const [pickup, setPickup] = useState("");
   const [destination, setDestination] = useState("");
   const [panelOpen, setPanelOpen] = useState(false);
+  const [vehiclePanel , setVehiclePanel] = useState(false)
   const panelRef = useRef(null);
-  const panelCloseRef = useRef(null)
+  const vehiclePanelRef = useRef(null);
+  const panelCloseRef = useRef(null);
   const submitHandler = (e) => {
     e.preventDefault();
     console.log(e.target.value);
@@ -20,25 +23,40 @@ const HomeScreen = () => {
       gsap.to(panelRef.current, {
         duration: 0.5,
         height: "70%",
-        padding: 24
+        padding: 24,
+        opacity: 1,
       });
       gsap.to(panelCloseRef.current, {
-          opacity: 1
-      })
+        opacity: 1,
+      });
     } else {
       gsap.to(panelRef.current, {
         duration: 0.5,
         height: "0%",
-        padding: 0
+        padding: 0,
+        opacity: 0,
       });
       gsap.to(panelCloseRef.current, {
-        opacity: 0
-    })
+        opacity: 0,
+      });
     }
   }, [panelOpen]);
 
+  useGSAP(() => {
+    if (vehiclePanel) {
+      gsap.to(vehiclePanelRef.current, {
+        transform: "translateY(0%)",
+      });
+    } else {
+      gsap.to(vehiclePanelRef.current, {
+        transform: "translateY(100%)",
+      });
+    }
+  }, [vehiclePanel]);
+
   return (
-    <div className="relative h-screen">
+    <div className="relative h-screen overflow-hidden">
+
       <img
         className="w-16 absolute left-5 top-5"
         src="https://upload.wikimedia.org/wikipedia/commons/c/cc/Uber_logo_2018.png"
@@ -55,13 +73,14 @@ const HomeScreen = () => {
 
       <div className=" flex flex-col justify-end absolute h-screen top-0 w-full ">
         <div className="h-[30%] bg-white relative  p-6">
-          <h5 className="absolute top-5 right-5 text-2xl opacity-0"
-          ref={panelCloseRef}
-          onClick={() => {
-            setPanelOpen(false)
-          }}
+          <h5
+            className="absolute top-5 right-5 text-2xl opacity-0"
+            ref={panelCloseRef}
+            onClick={() => {
+              setPanelOpen(false);
+            }}
           >
-          <i className="ri-arrow-down-wide-line"></i>
+            <i className="ri-arrow-down-wide-line"></i>
           </h5>
           <h4 className="text-2xl font-semibold">Find a trip</h4>
           <form
@@ -101,9 +120,14 @@ const HomeScreen = () => {
           </form>
         </div>
         <div ref={panelRef} className="h-[0%] bg-white  ">
-          <LocationSearchPanel />
+          <LocationSearchPanel setPanelOpen={setPanelOpen} setVehiclePanel={setVehiclePanel} />
         </div>
       </div>
+
+      <div ref={vehiclePanelRef} className="fixed w-full bg-white z-10 translate-y-full bottom-0 px-3 py-8">
+      <VehiclePanel setVehiclePanel={setVehiclePanel}/>
+      </div>
+
     </div>
   );
 };
