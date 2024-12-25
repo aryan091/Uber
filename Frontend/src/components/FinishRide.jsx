6751b6ba2d8ef7a4b544b9395243db957724e80b
Aryan Daftari <aryandaftari7@gmail.com>
2024-12-26 00:18:34 +0530

    Captain Screens UI Completed

---
+++ Frontend/src/components/FinishRide.jsx
@@ -0,0 +1,85 @@
+import React from 'react'
+import { Link } from "react-router-dom";
+
+const FinishRide = ({setFinishRide}) => {
+  return (
+    <div className="mt-4">
+    <h5
+      className="absolute top-5 right-5 text-2xl"
+      onClick={() => {
+        setFinishRide(false);
+      }}
+    >
+      <i className="ri-arrow-down-wide-line"></i>
+    </h5>
+
+    <h3 className="text-2xl font-semibold mb-8">Finish the Ride</h3>
+
+    <div className="flex items-center justify-between p-2 bg-yellow-400 rounded-lg mb-8">
+      <div className="flex items-center justify-start gap-4">
+        <img
+          className="h-12 rounded-full"
+          src="https://avatars.githubusercontent.com/u/59964730?v=4"
+          alt=""
+        />
+        <h4 className="text-lg font-medium">Aryan Daftari</h4>
+      </div>
+      <h5 className="text-lg font-semibold">2.2 Km</h5>
+    </div>
+
+    <div className="flex justify-between flex-col items-center gap-4">
+      <div className="w-full flex flex-col gap-4">
+        <div className="flex gap-4 items-center border-b-2 p-2">
+          <i className="ri-map-pin-2-fill text-lg"></i>
+          <div>
+            <h3 className="text-lg font-medium">526/11-A</h3>
+            <p className="text-sm -mt-1 text-gray-600">
+              Jagti Colony, Nagrota, Jammu
+            </p>
+          </div>
+        </div>
+
+        <div className="flex gap-4 items-center border-b-2 p-2">
+          <i className="ri-square-fill text-lg"></i>
+          <div>
+            <h3 className="text-lg font-medium">526/11-A</h3>
+            <p className="text-sm -mt-1 text-gray-600">
+              Jagti Colony, Nagrota, Jammu
+            </p>
+          </div>
+        </div>
+
+        <div className="flex gap-4 items-center p-2">
+          <i className="ri-cash-line text-lg"></i>
+          <div>
+            <h3 className="text-lg font-medium">₹163.45</h3>
+            <p className="text-sm -mt-1 text-gray-600">Cash</p>
+          </div>
+        </div>
+      </div>
+
+      <div className="flex items-center justify-between w-full gap-2">
+        
+          
+          
+
+<div className="flex flex-col items-center justify-between w-full gap-2">
+
+
+          <Link
+            to="/captainScreen"
+            className="w-full flex justify-center font-medium bg-black text-white py-3 mt-3"
+          >
+            Finish Ride
+          </Link>
+          <p className='text-xs text-wrap font-semibold text-center text-red-500 mt-10'>Click here to finish the ride if you have reached your destination</p>
+</div>
+          
+         
+      </div>
+    </div>
+  </div>
+  )
+}
+
+export default FinishRide
--- Frontend/src/pages/CaptainRiding.jsx
+++ Frontend/src/pages/CaptainRiding.jsx
@@ -1,10 +1,31 @@
-import React from 'react'
+import React, { useRef , useState} from 'react'
 import { Link } from 'react-router-dom';
 import "remixicon/fonts/remixicon.css";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import FinishRide from '../components/FinishRide';
 
 const CaptainRiding = () => {
+
+
+      const [finishRide , setFinishRide] = useState(false)
+    
+      const freshRideRef = useRef(null); 
+
+    useGSAP(() => {
+        if (finishRide) {
+          gsap.to(freshRideRef.current, {
+            transform: "translateY(0%)",
+          });
+        } else {
+          gsap.to(freshRideRef.current, {
+            transform: "translateY(100%)",
+          });
+        }
+      }, [finishRide]);
+
   return (
-    <div className="h-screen">
+    <div className="h-screen overflow-hidden">
 
       <div className="fixed p-6 top-0 flex items-center justify-between w-screen">
         <img
@@ -29,16 +50,22 @@ const CaptainRiding = () => {
       </div>
 
 
-      <div className="h-1/5 p-6 flex items-center justify-between relative bg-yellow-400 pt-10">
-      <h5 className='p-1 text-center width-screen absolute top-0'>
+      <div className="h-1/5 p-6 flex  items-center justify-between relative bg-yellow-400 pt-10">
+      <h5 onClick={() => {
+        setFinishRide(true)
+      }} className='p-1 text-center w-screen absolute top-0'>
         <i className='text-3xl text-gray-800 ri-arrow-up-wide-line'></i>
       </h5>
       <h4 className='text-xl font-semibold'>4 Km Away</h4>
-      <button className="w-1/2 font-medium bg-black text-white py-3 mt-3">Complete</button>
+      <button onClick={() => {
+        setFinishRide(true)
+      }} className="w-1/2 font-medium bg-black text-white py-3 mt-3">Complete</button>
         </div>
       
 
-
+        <div ref={freshRideRef}  className="fixed w-full bg-white z-10 translate-y-full bottom-0 px-3 py-8">
+<FinishRide setFinishRide={setFinishRide} />
+      </div>
 
 
     </div>
