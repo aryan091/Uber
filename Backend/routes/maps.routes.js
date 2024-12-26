bb85403883d0cc7d5f1cb4388a4eab36431dd50d
Aryan Daftari <aryandaftari7@gmail.com>
2024-12-26 21:28:58 +0530

    Configuring Get Suggestions Routes

--- Backend/routes/maps.routes.js
+++ Backend/routes/maps.routes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const authMiddleware = require("../middleware/auth.middleware");
 const {query} = require("express-validator");
-const {getCoordinates , getDistanceTime} = require("../controllers/map.controller");
+const {getCoordinates , getDistanceTime , getAutoCompleteSuggestions} = require("../controllers/map.controller");
 router.get("/get-coordinates", query("address").isLength({min:3}).withMessage("Address must be at least 3 characters long") ,authMiddleware.authUser, getCoordinates);
 
 router.get("/get-distance-time",
@@ -12,6 +12,11 @@ router.get("/get-distance-time",
     getDistanceTime
 )
 
+router.get("/get-suggestions",
+    query("input").isLength({min:3}).withMessage("Input must be at least 3 characters long"),
+    authMiddleware.authUser,
+    getAutoCompleteSuggestions
+)
 
 
 module.exports = router;
