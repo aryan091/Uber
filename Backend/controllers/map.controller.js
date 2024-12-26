65789166f6e59ce47af55eb35bb00bec860f3387
Aryan Daftari <aryandaftari7@gmail.com>
2024-12-26 21:28:02 +0530

    Get Auto Complete Suggestion of Addresses Service Created

--- Backend/controllers/map.controller.js
+++ Backend/controllers/map.controller.js
@@ -44,4 +44,21 @@ const getDistanceTime = async (req, res) => {
     }
 }
 
-module.exports = {getCoordinates , getDistanceTime};
+const getAutoCompleteSuggestions = async (req, res) => {
+
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+    
+        const {input} = req.query;
+    try {
+        const suggestions = await mapsService.getAutoCompleteSuggestions(input);
+        res.status(200).json({suggestions});
+    } catch (error) {
+        console.log(error);
+        res.status(400).json({error: 'Unable to fetch suggestions'});
+    }
+}
+
+module.exports = {getCoordinates , getDistanceTime , getAutoCompleteSuggestions};
--- Backend/services/maps.service.js
+++ Backend/services/maps.service.js
@@ -69,3 +69,228 @@ module.exports.getDistanceTime = async (origin, destination) => {
         throw new Error("Unable to fetch distance and time");
     }
 }
+
+/**
+ * @function getAutoCompleteSuggestions
+ * @description Get autocomplete suggestions for a given query from Google Place API.
+ * @param {string} address The query to get suggestions for.
+ * @returns {Promise<Array<Object>>} A promise that resolves with an array of prediction objects.
+ * @throws {Error} If the API call fails or the query is invalid.
+ */
+
+// {
+//     "suggestions": [
+//         {
+//             "description": "Jagraon, Punjab, India",
+//             "matched_substrings": [
+//                 {
+//                     "length": 3,
+//                     "offset": 0
+//                 }
+//             ],
+//             "place_id": "ChIJVzJqmpudEDkRfcknwGKcOt8",
+//             "reference": "ChIJVzJqmpudEDkRfcknwGKcOt8",
+//             "structured_formatting": {
+//                 "main_text": "Jagraon",
+//                 "main_text_matched_substrings": [
+//                     {
+//                         "length": 3,
+//                         "offset": 0
+//                     }
+//                 ],
+//                 "secondary_text": "Punjab, India"
+//             },
+//             "terms": [
+//                 {
+//                     "offset": 0,
+//                     "value": "Jagraon"
+//                 },
+//                 {
+//                     "offset": 9,
+//                     "value": "Punjab"
+//                 },
+//                 {
+//                     "offset": 17,
+//                     "value": "India"
+//                 }
+//             ],
+//             "types": [
+//                 "geocode",
+//                 "political",
+//                 "locality"
+//             ]
+//         },
+//         {
+//             "description": "Jagadhri, Haryana, India",
+//             "matched_substrings": [
+//                 {
+//                     "length": 3,
+//                     "offset": 0
+//                 }
+//             ],
+//             "place_id": "ChIJ69DAPe77DjkR0kfnMwLFBfE",
+//             "reference": "ChIJ69DAPe77DjkR0kfnMwLFBfE",
+//             "structured_formatting": {
+//                 "main_text": "Jagadhri",
+//                 "main_text_matched_substrings": [
+//                     {
+//                         "length": 3,
+//                         "offset": 0
+//                     }
+//                 ],
+//                 "secondary_text": "Haryana, India"
+//             },
+//             "terms": [
+//                 {
+//                     "offset": 0,
+//                     "value": "Jagadhri"
+//                 },
+//                 {
+//                     "offset": 10,
+//                     "value": "Haryana"
+//                 },
+//                 {
+//                     "offset": 19,
+//                     "value": "India"
+//                 }
+//             ],
+//             "types": [
+//                 "political",
+//                 "locality",
+//                 "geocode"
+//             ]
+//         },
+//         {
+//             "description": "Jagdalpur, Chhattisgarh, India",
+//             "matched_substrings": [
+//                 {
+//                     "length": 3,
+//                     "offset": 0
+//                 }
+//             ],
+//             "place_id": "ChIJ62uuxWsSMDoRpyuVEKzU5Mo",
+//             "reference": "ChIJ62uuxWsSMDoRpyuVEKzU5Mo",
+//             "structured_formatting": {
+//                 "main_text": "Jagdalpur",
+//                 "main_text_matched_substrings": [
+//                     {
+//                         "length": 3,
+//                         "offset": 0
+//                     }
+//                 ],
+//                 "secondary_text": "Chhattisgarh, India"
+//             },
+//             "terms": [
+//                 {
+//                     "offset": 0,
+//                     "value": "Jagdalpur"
+//                 },
+//                 {
+//                     "offset": 11,
+//                     "value": "Chhattisgarh"
+//                 },
+//                 {
+//                     "offset": 25,
+//                     "value": "India"
+//                 }
+//             ],
+//             "types": [
+//                 "geocode",
+//                 "political",
+//                 "locality"
+//             ]
+//         },
+//         {
+//             "description": "Jageshwar Dham, Uttarakhand, India",
+//             "matched_substrings": [
+//                 {
+//                     "length": 3,
+//                     "offset": 0
+//                 }
+//             ],
+//             "place_id": "ChIJ3y-Lb-fOoDkRhphMrdfytQw",
+//             "reference": "ChIJ3y-Lb-fOoDkRhphMrdfytQw",
+//             "structured_formatting": {
+//                 "main_text": "Jageshwar Dham",
+//                 "main_text_matched_substrings": [
+//                     {
+//                         "length": 3,
+//                         "offset": 0
+//                     }
+//                 ],
+//                 "secondary_text": "Uttarakhand, India"
+//             },
+//             "terms": [
+//                 {
+//                     "offset": 0,
+//                     "value": "Jageshwar Dham"
+//                 },
+//                 {
+//                     "offset": 16,
+//                     "value": "Uttarakhand"
+//                 },
+//                 {
+//                     "offset": 29,
+//                     "value": "India"
+//                 }
+//             ],
+//             "types": [
+//                 "political",
+//                 "locality",
+//                 "geocode"
+//             ]
+//         },
+//         {
+//             "description": "Jagtial, Telangana, India",
+//             "matched_substrings": [
+//                 {
+//                     "length": 3,
+//                     "offset": 0
+//                 }
+//             ],
+//             "place_id": "ChIJcQeEToUTzTsRlbg8DHlsw9Q",
+//             "reference": "ChIJcQeEToUTzTsRlbg8DHlsw9Q",
+//             "structured_formatting": {
+//                 "main_text": "Jagtial",
+//                 "main_text_matched_substrings": [
+//                     {
+//                         "length": 3,
+//                         "offset": 0
+//                     }
+//                 ],
+//                 "secondary_text": "Telangana, India"
+//             },
+//             "terms": [
+//                 {
+//                     "offset": 0,
+//                     "value": "Jagtial"
+//                 },
+//                 {
+//                     "offset": 9,
+//                     "value": "Telangana"
+//                 },
+//                 {
+//                     "offset": 20,
+//                     "value": "India"
+//                 }
+//             ],
+//             "types": [
+//                 "geocode",
+//                 "political",
+//                 "locality"
+//             ]
+//         }
+//     ]
+// }
+module.exports.getAutoCompleteSuggestions = async (input) => {
+    if(!input) throw new Error("Query is required");
+    try {
+        const response = await axios.get(`https://maps.googleapis.com/maps/api/place/autocomplete/json?input=${input}&key=${process.env.GOOGLE_MAPS_API_KEY}`);
+
+        if(response.data.status !== "OK") throw new Error("Unable to fetch suggestions");
+
+        return response.data.predictions;
+    } catch (error) {
+        throw new Error("Unable to fetch suggestions");
+    }
+}
