a119a8b3d4ae8c4a9e2a8766105caf6ce6c1cfad
Aryan Daftari <aryandaftari7@gmail.com>
2024-12-26 21:13:26 +0530

    Configuring Get Distance and Time Routes

--- Backend/routes/maps.routes.js
+++ Backend/routes/maps.routes.js
@@ -2,7 +2,16 @@ const express = require("express");
 const router = express.Router();
 const authMiddleware = require("../middleware/auth.middleware");
 const {query} = require("express-validator");
-const {getCoordinates} = require("../controllers/map.controller");
+const {getCoordinates , getDistanceTime} = require("../controllers/map.controller");
 router.get("/get-coordinates", query("address").isLength({min:3}).withMessage("Address must be at least 3 characters long") ,authMiddleware.authUser, getCoordinates);
+
+router.get("/get-distance-time",
+    query("origin").isLength({min:3}).withMessage("Origin must be at least 3 characters long"),
+    query("destination").isLength({min:3}).withMessage("Destination must be at least 3 characters long"),
+    authMiddleware.authUser,
+    getDistanceTime
+)
+
+
 
 module.exports = router;
