ea1df1b74f6a7edef4578633506df88e80b1f05a
Aryan Daftari <aryandaftari7@gmail.com>
2024-12-28 01:10:30 +0530

    Configuring Rides Routes in Main App

--- Backend/app.js
+++ Backend/app.js
@@ -10,6 +10,7 @@ const connectDB = require("./db/db");
 const userRoutes = require("./routes/user.routes");
 const captainRoutes = require("./routes/captain.routes");
 const mapRoutes = require("./routes/maps.routes");
+const rideRoutes = require("./routes/rides.routes");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -25,5 +26,6 @@ app.get("/", (req, res) => {
 app.use("/users", userRoutes);
 app.use("/captains", captainRoutes);
 app.use("/maps", mapRoutes);
+app.use("/rides", rideRoutes);
 
 module.exports = app;
